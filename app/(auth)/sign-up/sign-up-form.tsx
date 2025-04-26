a884ca2c7563bf58b421605f141dab79e6a37882
maminizer <amine.mami@esprit.tn>
2025-04-26 11:30:46 +0100

    feat: implement new pages for cart, product creation, user profile, and shipping address with necessary components and authentication

--- app/(auth)/sign-up/sign-up-form.tsx
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -1,85 +1,92 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { signUpDefaultValues } from "@/lib/constants";
-import Link from "next/link";
-import { signUpUser } from "@/lib/actions/user.actions";
-import { useActionState } from "react";
-import { useFormStatus } from "react-dom";
-import { useSearchParams } from "next/navigation";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { signUpDefaultValues } from '@/lib/constants';
+import Link from 'next/link';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { signUpUser } from '@/lib/actions/user.actions';
+import { useSearchParams } from 'next/navigation';
 
 const SignUpForm = () => {
   const [data, action] = useActionState(signUpUser, {
     success: false,
-    message: "",
+    message: '',
   });
 
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/";
-  const SignInButton = () => {
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+  const SignUpButton = () => {
     const { pending } = useFormStatus();
+
     return (
-      <Button disabled={pending} className="w-full" variant="default">
-        {pending ? "Submitting..." : "Sign Up"}
+      <Button disabled={pending} className='w-full' variant='default'>
+        {pending ? 'Submitting...' : 'Sign Up'}
       </Button>
     );
   };
+
   return (
     <form action={action}>
-      <input type="hidden" name="callbackUrl" value={callbackUrl} />
-      <div className="space-y-6">
+      <input type='hidden' name='callbackUrl' value={callbackUrl} />
+      <div className='space-y-6'>
         <div>
-          <Label htmlFor="name">Name</Label>
+          <Label htmlFor='email'>Name</Label>
           <Input
-            id="name"
-            name="name"
-            type="text"
-            autoComplete="name"
+            id='name'
+            name='name'
+            type='text'
+            autoComplete='name'
+            defaultValue={signUpDefaultValues.name}
+          />
+        </div>
+        <div>
+          <Label htmlFor='email'>Email</Label>
+          <Input
+            id='email'
+            name='email'
+            type='text'
+            autoComplete='email'
             defaultValue={signUpDefaultValues.email}
           />
         </div>
         <div>
-          <Label htmlFor="email">Email</Label>
+          <Label htmlFor='password'>Password</Label>
           <Input
-            id="email"
-            name="email"
-            type="text"
-            autoComplete="email"
-            defaultValue={signUpDefaultValues.email}
-          />
-        </div>
-        <div>
-          <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="password"
+            id='password'
+            name='password'
+            type='password'
+            required
+            autoComplete='password'
             defaultValue={signUpDefaultValues.password}
           />
         </div>
         <div>
-          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Label htmlFor='confirmPassword'>Confirm Password</Label>
           <Input
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            autoComplete="confirmPassword"
+            id='confirmPassword'
+            name='confirmPassword'
+            type='password'
+            required
+            autoComplete='confirmPassword'
             defaultValue={signUpDefaultValues.confirmPassword}
           />
         </div>
         <div>
-          <SignInButton />
+          <SignUpButton />
         </div>
+
         {data && !data.success && (
-          <div className="text-center text-destructive">{data.message}</div>
+          <div className='text-center text-destructive'>{data.message}</div>
         )}
-        <div className="text-sm text-center text-muted-foreground">
-          Already have an account?{" "}
-          <Link href="/sign-in" target="_self" className="text-blue-600">
-            Sign up
+
+        <div className='text-sm text-center text-muted-foreground'>
+          Already have an account?{' '}
+          <Link href='/sign-in' target='_self' className='link'>
+            Sign In
           </Link>
         </div>
       </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,28 @@
-import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ProductList from '@/components/shared/product/product-list';
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions';
+import ProductCarousel from '@/components/shared/product/product-carousel';
+import ViewAllProductsButton from '@/components/view-all-products-button';
+import IconBoxes from '@/components/icon-boxes';
+import DealCountdown from '@/components/deal-countdown';
 
-const HomePage = async () => {
+const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
-      <ProductList data={latestProducts} title="Newest Arrivals" />
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
+      <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
 
-export default HomePage;
+export default Homepage;
